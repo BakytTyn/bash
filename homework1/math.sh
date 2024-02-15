7b0fd03162d13c1d8f28eccca6c7e8a0834b7538
BakytTyn <bakyt.tynalieva@gmail.com>
2024-02-15 05:17:15 +0000

    added HomeWork

---
+++ homework1/math.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a=10
+b=2
+
+echo a+b=  $(($a+$b))
+
+echo a-b=  $(($a-$b))
+
+echo a*b=  $(($a*$b))
+
+echo a/b=  $(($a/$b))
+
---
+++ homework1/user_input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+read -n 10 -p "Enter your phone nunber: "  phone number
+echo
+read -n 3 -p "Enter your age:  " age
+echo
+read -n 2 -p "Enter your state: " state
+echo
+echo "Tims pnone number is $phone number, Tims age is $age, Tims live in $state."
+
+
